3f3c220e68b7674f48df7e6cd1b2290056c5873f
Mohamed Elshenawy <alshenawy10203022@gmail.com>
2025-07-08 18:22:59 +0300

    refactor(Book.ts)!: change Book to be abstract

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,8 +1,14 @@
 import crypto from "crypto";
 
-export default class Book {
+export default abstract class Book {
     isbn: crypto.UUID;
     title: string;
     year: number;
     price: number;
+
+    abstract buy(purchase: {
+        quantity: number;
+        customer_email: string;
+        customer_address: string;
+    }): number;
 }

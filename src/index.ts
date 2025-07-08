28ade365c0d4c01b67521523818a5f13d8cb4a10
Mohamed Elshenawy <alshenawy10203022@gmail.com>
2025-07-08 20:16:45 +0300

    feat: add usage example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-function main() {
-  console.log("Hello, World!");
-}
+import { PhysicalBookBuilder } from "./book/models/PhysicalBook";
+import BookStore from "./bookstore/BookStore";
 
-main();
+const store = new BookStore();
+const isbn = crypto.randomUUID();
+
+const book = new PhysicalBookBuilder()
+    .setISBN(isbn)
+    .setPrice(10)
+    .setStock(1)
+    .setTitle("Land of Zicola")
+    .setYear(2010)
+    .build();
+
+store.addBook(book);
+
+const quantity = 1;
+
+store.buyBook(
+    isbn,
+    quantity,
+    "Building B143, 2nd Floor Smart Village",
+    "foo@fawry.com",
+);
